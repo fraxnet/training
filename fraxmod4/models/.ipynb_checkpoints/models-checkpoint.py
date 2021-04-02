3d8097e1d1a7d077f5b03a71c6f98055690a503c
fraxnet <firas@fraxnet.com>
2021-04-02 22:47:34 +0000

    [ADD] fraxmod4: add the start_datetime field to the mdel fraxmod4

--- fraxmod4/models/.ipynb_checkpoints/models-checkpoint.py
+++ fraxmod4/models/.ipynb_checkpoints/models-checkpoint.py
@@ -11,6 +11,8 @@ class fraxmod4(models.Model):
     value = fields.Integer()
     value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
+    start_datetime = fields.Datetime('Start time', default=lambda self: fields.Datetime.now())
+
 
     @api.depends('value')
     def _value_pc(self):
